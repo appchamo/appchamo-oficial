46936587bcfc2d351f8c37f62a1ad399aa40ce5c
Rafael Nogueira <chamotecnologia@gmail.com>
2026-02-19 02:29:56 -0300

    fix: adjust vercel rewrites to allow sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,9 @@
 /// <reference lib="webworker" />
 
+// 🔥 PRIMEIRO: carregar OneSignal
+self.importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// 🔥 DEPOIS: Workbox
 import { precacheAndRoute } from "workbox-precaching";
 
-// Isso é injetado automaticamente pelo Vite
 precacheAndRoute(self.__WB_MANIFEST);
-
-// 🔥 Integração do OneSignal dentro do mesmo Service Worker
-self.importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
